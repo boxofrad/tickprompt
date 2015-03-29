c150957e8ba936ff77d18f654ae2b8fdd391cb51
Daniel Upton <daniel@floppy.co>
2015-03-29 13:53:43 +0100

    We no longer care about expected daily hours

--- config.go
+++ config.go
@@ -9,23 +9,20 @@ import (
 
 const (
 	// Environment variables
-	SUBSCRIPTION_ID      = "TICKPROMPT_SUBSCRIPTION_ID"
-	API_TOKEN            = "TICKPROMPT_API_TOKEN"
-	USER_ID              = "TICKPROMPT_USER_ID"
-	EXPECTED_DAILY_HOURS = "TICKPROMPT_EXPECTED_DAILY_HOURS"
-	CACHE_TTL            = "TICKPROMPT_CACHE_TTL"
+	SUBSCRIPTION_ID = "TICKPROMPT_SUBSCRIPTION_ID"
+	API_TOKEN       = "TICKPROMPT_API_TOKEN"
+	USER_ID         = "TICKPROMPT_USER_ID"
+	CACHE_TTL       = "TICKPROMPT_CACHE_TTL"
 
 	// Defaults
-	DEFAULT_EXPECTED_DAILY_HOURS = 7.5
-	DEFAULT_CACHE_TTL            = 900
+	DEFAULT_CACHE_TTL = 900
 )
 
 type Config struct {
-	SubscriptionId     int
-	ApiToken           string
-	UserId             int
-	ExpectedDailyHours float32
-	CacheTTL           time.Duration
+	SubscriptionId int
+	ApiToken       string
+	UserId         int
+	CacheTTL       time.Duration
 }
 
 func LoadConfig() (*Config, error) {
@@ -47,20 +44,6 @@ func LoadConfig() (*Config, error) {
 		return nil, err
 	}
 
-	expectedStr := os.Getenv(EXPECTED_DAILY_HOURS)
-
-	var expectedDailyHours float64
-
-	if expectedStr == "" {
-		expectedDailyHours = DEFAULT_EXPECTED_DAILY_HOURS
-	} else {
-		expectedDailyHours, err = strconv.ParseFloat(expectedStr, 32)
-
-		if err != nil {
-			return nil, err
-		}
-	}
-
 	cacheTTL, err := getEnvInt(CACHE_TTL)
 
 	if err != nil {
@@ -68,11 +51,10 @@ func LoadConfig() (*Config, error) {
 	}
 
 	return &Config{
-		SubscriptionId:     subscriptionId,
-		ApiToken:           apiToken,
-		UserId:             userId,
-		ExpectedDailyHours: float32(expectedDailyHours),
-		CacheTTL:           time.Duration(cacheTTL) * time.Second,
+		SubscriptionId: subscriptionId,
+		ApiToken:       apiToken,
+		UserId:         userId,
+		CacheTTL:       time.Duration(cacheTTL) * time.Second,
 	}, nil
 }
 
